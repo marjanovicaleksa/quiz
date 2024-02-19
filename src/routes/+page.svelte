48abd808e4db716aec99ba3f83601d13b5aa122e
marjanovicaleksa <aleksamarjanovic4@gmail.com>
2024-02-19 12:10:45 +0100

    prezentaciona logika

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Question from "./pitanje.svelte"
+</script>
+<h1>Kviz</h1>
+<Question
+    question="kad je bio kosovski boj?";
+    correctAnswer={1389}
+    incorrectAnswers={[1390, 1391, 1392]}
+/>
---
+++ src/routes/pitanje.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let question;
+    export let correctAnswer;
+    export let incorrectAnswers;
+
+    const shuffle = (array) => array.sort(()=>Math.random()-0.5);
+    let possibleAnswers = [...incorrectAnswers, correctAnswer];
+
+    let correctIndex = possibleAnswers.indexOf(correctAnswer);
+
+    const handleClick = (answer) =>{
+        if (answer===correctAnswer){
+            alert("Correct!");
+        }
+        else{
+            alert("Wrong! The correct answer is "+correctAnswer+".");
+        }
+    }
+</script>
+
+<div>
+    <h3>{@html question}</h3>
+    <div id="answers">
+        <!--prikazi sve ponudjene kao button-e-->
+        {#each possibleAnswers as pa, i}
+            <button on:click={()=>handleClick(pa)}>{pa}</button>
+
+        {/each}
+    </div>
+</div>
